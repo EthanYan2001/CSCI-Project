a8ce5bff2fc60e051f98edcb5385bec5ee6334d6
EthanYan2001 <eyan03@nyit.edu>
2022-05-07 17:50:11 -0400

    Uploading Firebase Database

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAsFvK2xBKR9dpZLtWjvjg6DF6gLNddUi0",
+    authDomain: "csci318-2790f.firebaseapp.com",
+    projectId: "csci318-2790f",
+    storageBucket: "csci318-2790f.appspot.com",
+    messagingSenderId: "798576206313",
+    appId: "1:798576206313:web:98e4bd21350d131a392b78"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  
